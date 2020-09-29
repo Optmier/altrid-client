b93825e5215ab1656d7590862701f2d980776adc
jun-young <jun094@naver.com>
2020-09-29 15:39:57 +0900

    폴더 구조 완성

---
+++ src/components/essentials/ClassShare/Share.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Share() {
+    return <div>과제 공유 페이지입니다.</div>;
+}
+
+export default Share;
---
+++ src/pages/Class.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Class() {
+    return <div>클래스페이지입니다.</div>;
+}
+
+export default Class;
