804781cbc831189e4f582d49bc4e5af7d8ea2957
jun-young <jun094@naver.com>
2020-10-01 18:36:22 +0900

    left nav 라우터 분리 완료

--- src/pages/Class.js
+++ src/pages/Class.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import LeftNav from '../components/essentials/LeftNav';
 import '../styles/class.scss';
+import Draft from '../components/ClassDraft/Draft';
+import Manage from '../components/ClassManage/Manage';
+import Share from '../components/ClassShare/Share';
 
-function Class() {
+const ClassPageSwitcher = ({ classPageName }) => {
+    switch (classPageName) {
+        case '/class/draft':
+            return <Draft />;
+        case '/class/manage':
+            return <Manage />;
+        case '/class/share':
+            return <Share />;
+
+        default:
+            return (
+                <>
+                    존재하지 않는 페이지입니다. <br /> 404 :(
+                </>
+            );
+    }
+};
+
+function Class({ history }) {
+    const classPageName = history.location['pathname'];
+
     return (
         <>
             <LeftNav />
             <div className="class-page-root">
-                <div className="class-page-wrapper">클래스페이지입니다.</div>
+                <div className="class-page-wrapper">
+                    <p>클래스페이지입니다.</p>
+                    <ClassPageSwitcher classPageName={classPageName} />
+                </div>
             </div>
         </>
     );
