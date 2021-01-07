7f07a06bd9c5a8dae08fd0bc0ae0353553af5c88
jun-young <jun094@naver.com>
2021-01-07 22:36:45 +0900

    calibration

--- src/components/EyetrackerStep/StepCalibration.js
+++ src/components/EyetrackerStep/StepCalibration.js
@@ -1,7 +1,142 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function StepCalibration() {
-    return <div className="eyetrack-step-calibration">20,</div>;
+const Assistance = styled.div``;
+const CalibDot = styled.div`
+    background-color: #ffff00;
+    border: 2px solid black;
+    border-radius: 50%;
+    box-sizing: border-box;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+
+    &.top-left {
+        top: 12px;
+        left: 12px;
+    }
+
+    &.top-center {
+        top: 12px;
+        left: calc(50% - 10px);
+    }
+
+    &.top-right {
+        top: 12px;
+        right: 12px;
+    }
+
+    &.middle-left {
+        top: calc(50% - 10px);
+        left: 12px;
+    }
+
+    &.middle-center {
+        top: calc(50% - 10px);
+        left: calc(50% - 10px);
+    }
+
+    &.middle-right {
+        top: calc(50% - 10px);
+        right: 12px;
+    }
+
+    &.bottom-left {
+        bottom: 12px;
+        left: 12px;
+    }
+
+    &.bottom-center {
+        bottom: 12px;
+        left: calc(50% - 10px);
+    }
+
+    &.bottom-right {
+        bottom: 12px;
+        right: 12px;
+    }
+
+    &:hover {
+        background-color: #dada00;
+    }
+
+    &.ok {
+        background-color: #00ac75;
+        pointer-events: none;
+    }
+`;
+
+function StepCalibration({ onCalibDotClick, onCalibDotHover, onCalibDotLeave }) {
+    return (
+        <div className="eyetrack-step-calibration">
+            <Assistance className="eyetracker-calib-assistance">
+                <CalibDot
+                    className="calib-dots top-left"
+                    data-type="top_left"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots top-center"
+                    data-type="top_center"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots top-right"
+                    data-type="top_right"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots middle-left"
+                    data-type="middle_left"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots middle-center"
+                    data-type="middle_center"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots middle-right"
+                    data-type="middle_right"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots bottom-left"
+                    data-type="bottom_left"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots bottom-center"
+                    data-type="bottom_center"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+                <CalibDot
+                    className="calib-dots bottom-right"
+                    data-type="bottom_right"
+                    onClick={onCalibDotClick}
+                    onMouseOver={onCalibDotHover}
+                    onMouseLeave={onCalibDotLeave}
+                ></CalibDot>
+            </Assistance>
+        </div>
+    );
 }
 
 export default StepCalibration;
--- src/components/EyetrackerStep/StepCameraCheck.js
+++ src/components/EyetrackerStep/StepCameraCheck.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function StepCameraCheck() {
+function StepCameraCheck({ setWebgazerLoaded }) {
+    useEffect(() => {
+        setWebgazerLoaded(false);
+    }, []);
     return (
         <div className="eyetrack-step-camera">
             <div className="eyetrack-step-header">가이드 박스 안에 얼굴을 위치시켜주세요.</div>
-            <div className="contents-box">카메라 체크 함수 발동</div>
+            <div className="contents-box"> </div>
         </div>
     );
 }
