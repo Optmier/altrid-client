9d8117ee85c54be43cdf0edd18f0afe8f785bb26
jun-young <jun094@naver.com>
2021-01-19 17:41:55 +0900

    유형별 분석 안될 때, chart 렌더링 변경

--- src/components/ClassReport/ReportClass.js
+++ src/components/ClassReport/ReportClass.js
@@ -63,10 +63,30 @@ const StudentCardHeader = styled.div`
         }
     }
 `;
+const LimitFuncWrapper = styled.div`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    left: 27%;
+    top: 40%;
+    font-size: 1.2rem;
+    font-weight: 500;
+    z-index: 1000;
+
+    & svg {
+        margin-right: 15px;
+    }
+
+    @media (min-width: 0) and (max-width: 663px) {
+        font-size: 0.85rem;
+        text-align: center;
+        & svg {
+            margin-right: 5px;
+        }
+    }
+`;
 
 function ReportClass({ match, history }) {
-    console.log(match.params);
-
     const { num, activedNum } = match.params;
     const dispatch = useDispatch();
     const serverdate = useSelector((state) => state.RdxServerDate);
@@ -617,17 +637,32 @@ function ReportClass({ match, history }) {
                             <div>
                                 <select name="chart-option" onChange={handleSelect}>
                                     <option value="0">문제별 정답률</option>
-                                    {achievesForTypes.value >= 100 ? <option value="1">유형별 정답률</option> : null}
+                                    <option value="1">유형별 정답률</option>
                                 </select>
                             </div>
                         </div>
                         <div className="graph-box">
                             {selectState === '0' ? (
                                 <ColumnChartProblem datas={avgScoresOfNumber} />
-                            ) : (
+                            ) : achievesForTypes.value >= 100 ? (
                                 <ColumnChartType
                                     datas={achievesForTypes.allExists.map((e) => ({ ...e, score: averageScoresOfType[e.category] }))}
                                 />
+                            ) : (
+                                <>
+                                    <LimitFuncWrapper>
+                                        <svg id="Warning" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                                            <path
+                                                id="패스_35"
+                                                data-name="패스 35"
+                                                d="M8,0a8,8,0,1,0,8,8A8.024,8.024,0,0,0,8,0ZM9.1,12.2H6.9V10.3H9.2v1.9Zm.1-7.4L8.6,9.2H7.4L6.8,4.8v-1H9.3v1Z"
+                                                fill="#605f60"
+                                            />
+                                        </svg>
+                                        과제를 더 다양한 문제로 만들어주세요!
+                                    </LimitFuncWrapper>
+                                    <ColumnChartType datas={0} />
+                                </>
                             )}
                         </div>
                     </section>
