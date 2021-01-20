3bcbf0e893be006df65f6738e0f045fc1b3a4149
jun-young <jun094@naver.com>
2021-01-20 21:07:46 +0900

    학생별 리포트 반응형

--- src/components/ClassReport/StudentTypeScore.js
+++ src/components/ClassReport/StudentTypeScore.js
@@ -6,37 +6,97 @@ import ColumnChart from '../essentials/ColumnChart';
 const StyleStudentTypeScore = styled.div`
     box-sizing: border-box;
     background-color: white;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
+
+    border-radius: 11px;
     padding: 30px 32px;
     width: 100%;
     box-sizing: border-box;
-    overflow: auto;
+
     display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
+    flex-direction: column;
 
-    & .chart-right {
-        width: 40%;
-    }
+    & .header-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
 
-    & .chart-right {
-        width: 60%;
+        & .header-center {
+            display: flex;
+            align-items: center;
+            font-size: 0.94rem;
+            font-weight: 400;
+            color: #171717b3;
+
+            & > span {
+                display: flex;
+                align-items: center;
+            }
+            & span + span {
+                margin-left: 15px;
+            }
+            & .circle {
+                width: 11px;
+                height: 11px;
+                border-radius: 50%;
+                margin-left: 5px;
+            }
+            & .circle.student {
+                background-color: #351e85;
+            }
+            & .circle.class {
+                background-color: #68dea6;
+            }
+        }
+        & select {
+            cursor: pointer;
+            background: url(/bg_images/Vector.png) no-repeat 92% 50%;
+            width: 160px;
+            padding: 0.4rem 0.8rem;
+            font-family: inherit;
+            font-size: 1.001rem;
+            border: none;
+            border: 1px solid #707070;
+            border-radius: 0px;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            outline: none;
+        }
     }
 `;
 
-function StudentTypeScore({ enabled, current, total }) {
+function StudentTypeScore({ enabled, current, total, typeSelectState, handleTypeSelect }) {
     const [sortedEnableCategories] = useState(enabled.sort((a, b) => a.category - b.category));
+
     return (
         <StyleStudentTypeScore>
-            <div className="chart-left">
-                <RadarChart objDatas={sortedEnableCategories.map((e) => ({ ...e, score: current[e.category] }))} />
+            <div className="header-wrapper">
+                <div className="header-center">
+                    <span>
+                        학생 시간 <div className="circle student"></div>
+                    </span>
+                    <span>
+                        반 평균 <div className="circle class"></div>
+                    </span>
+                </div>
+
+                <select name="chart-option" onChange={handleTypeSelect}>
+                    <option value="0">방사형 그래프</option>
+                    <option value="1">막대형 그래프</option>
+                </select>
             </div>
-            <div className="chart-right">
-                <ColumnChart
-                    currentObjs={sortedEnableCategories.map((e) => ({ ...e, score: current[e.category] }))}
-                    averageObjs={sortedEnableCategories.map((e) => ({ ...e, score: total[e.category] }))}
-                />
+
+            <div className="chart-wrapper">
+                {typeSelectState === '0' ? (
+                    <RadarChart objDatas={sortedEnableCategories.map((e) => ({ ...e, score: current[e.category] }))} />
+                ) : (
+                    <ColumnChart
+                        currentObjs={sortedEnableCategories.map((e) => ({ ...e, score: current[e.category] }))}
+                        averageObjs={sortedEnableCategories.map((e) => ({ ...e, score: total[e.category] }))}
+                    />
+                )}
             </div>
         </StyleStudentTypeScore>
     );
