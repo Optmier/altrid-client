cd0c623e65a2dcb7bf590eead5c912285fd1e2f8
jun-young <jun094@naver.com>
2021-03-01 18:21:52 +0900

    fail 페이지 쿼리스트링을 통한 오류코드 렌더링 완료

--- src/components/Payment/PayFail.js
+++ src/components/Payment/PayFail.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import '../../styles/pay_state.scss';
+import queryString from 'query-string';
+import { withRouter } from 'react-router-dom';
+import Error from '../../pages/Error';
 
-function PaySuccess() {
+function PaySuccess({ location }) {
+    //오류 코드가 없으면 404 페이지 렌더링
+    if (!queryString.parse(location.search).message) return <Error />;
     return (
         <>
             <div className="header-text">
                 죄송합니다 ! <br />
                 카드 등록이 실패하였습니다:(
             </div>
-            <div className="fail-text">오류코드 : 잘못된 카드 번호입니다.</div>
+            <div className="fail-text">오류코드 : {queryString.parse(location.search).message}</div>
             <div className="fail-info">
                 <div className="info-header">
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M14.4 0H1.6C0.72 0 0.00799999 0.72 0.00799999 1.6L0 16L3.2 12.8H14.4C15.28 12.8 16 12.08 16 11.2V1.6C16 0.72 15.28 0 14.4 0ZM8.8 7.2H7.2V2.4H8.8V7.2ZM8.8 10.4H7.2V8.8H8.8V10.4Z"
                             fill="#575555"
-                            fill-opacity="0.62"
+                            fillOpacity="0.62"
                         />
                     </svg>
                     문의가 필요하신가요?
@@ -26,4 +31,4 @@ function PaySuccess() {
     );
 }
 
-export default PaySuccess;
+export default withRouter(PaySuccess);
