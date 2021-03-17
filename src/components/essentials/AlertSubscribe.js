6226f3baf13114611da18e7d2da95ed1a417e61c
jun-young <jun094@naver.com>
2021-03-17 15:29:12 +0900

    구독설정 alert switch-case 완성

--- src/components/essentials/AlertSubscribe.js
+++ src/components/essentials/AlertSubscribe.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const StyleDiv = styled.div`
     position: fixed;
@@ -56,17 +58,104 @@ const StyleDiv = styled.div`
     }
 `;
 
-function AlertSubscribe() {
+const switchAlertText = (pathname, restricted) => {
+    // const {
+    //     analysisPattern,
+    //     analysisTime,
+    //     analysisType,
+    //     classCreation,
+    //     classReport,
+    //     editorCreation,
+    //     eyetrack,
+    //     fileCreation,
+    //     studentInvited,
+    //     studentReport,
+    //     teacherInvited,
+    //     timeLimit,
+    //     videoLecture,
+    // } = restricted;
+
+    const fileCreation = true;
+    const eyetrack = false;
+
+    switch (pathname) {
+        case '/':
+            if (fileCreation && !eyetrack)
+                return <div className="text">'파일 업로드를 통한 과제 생성'과 '시선흐름 과제 게시' 기능이 제한되었습니다.</div>;
+            else if (!fileCreation && eyetrack)
+                return <div className="text">'파일 업로드를 통한 과제 생성'과 '시선흐름 과제 게시' 기능이 제한되었습니다.</div>;
+            else return <div className="text">'파일 업로드를 통한 과제 생성'과 '시선흐름 과제 게시' 기능이 제한되었습니다.</div>;
+
+        case '/main-draft':
+            return <div className="text">'파일 업로드를 통한 과제 생성'과 '시선흐름 과제 게시' 기능이 제한되었습니다.</div>;
+        default:
+            break;
+    }
+};
+
+function AlertSubscribe({ history }) {
+    const { pathname } = history.location;
+
+    const [renderState, setRenderState] = useState(false);
+    const [text, setText] = useState('');
+
+    const { userType } = useSelector((state) => state.RdxSessions);
+    const restricted = useSelector((state) => state.planInfo.restricted);
+    const {
+        analysisPattern,
+        analysisTime,
+        analysisType,
+        classCreation,
+        classReport,
+        editorCreation,
+        eyetrack,
+        fileCreation,
+        studentInvited,
+        studentReport,
+        teacherInvited,
+        timeLimit,
+        videoLecture,
+    } = restricted;
+
     const handleAlert = () => {};
+
+    useEffect(() => {
+        console.log(pathname, fileCreation, eyetrack);
+        switch (pathname) {
+            case '/':
+                if (teacherInvited) {
+                    setRenderState(true);
+                    setText('선생님 초대');
+                }
+                break;
+            case '/main-draft':
+                if (fileCreation === true && eyetrack === false) {
+                    setRenderState(true);
+                    setText(`'파일 업로드를 통한 과제 생성'`);
+                } else if (fileCreation === false && eyetrack === true) {
+                    setRenderState(true);
+                    setText(`'시선흐름 과제 게시'`);
+                } else if (fileCreation === true && eyetrack === true) {
+                    setRenderState(true);
+                    setText(`'파일 업로드를 통한 과제 생성' 기능과 '시선흐름 과제 게시'`);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }, [pathname, restricted]);
+
+    if (!renderState || userType === 'students') return null;
     return (
         <StyleDiv>
             <div></div>
             <div className="middle">
-                <div className="text">'파일 업로드를 통한 과제 생성'과 '시선흐름 과제 게시' 기능이 제한되었습니다.</div>
+                <div className="text">{text} 기능이 제한되었습니다.</div>
                 <button>
                     구독 설정
                     <svg width="19" height="7" viewBox="0 0 19 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 1H16L11.2743 6" stroke="white" stroke-width="2" />
+                        <path d="M0 1H16L11.2743 6" stroke="white" strokeWidth="2" />
                     </svg>
                 </button>
             </div>
@@ -80,4 +169,4 @@ function AlertSubscribe() {
     );
 }
 
-export default AlertSubscribe;
+export default withRouter(AlertSubscribe);
